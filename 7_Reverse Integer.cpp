c316d5c78dc39fbc97172f175aa25dde653429aa
AADITYARAJ SINGH CHOUHAN <94135988+Adityacsbs@users.noreply.github.com>
2023-08-23 04:10:58 +0530

    Create 7_Reverse Integer.cpp

---
+++ 7_Reverse Integer.cpp
@@ -0,0 +1,20 @@
+//problem link :- https://leetcode.com/problems/reverse-integer/description/
+
+class Solution {
+public:
+    int reverse(int x) {
+        long long rev = 0;
+        while(x!=0){
+            int a = x%10;
+            rev = rev * 10 + a;
+            x=x/10;
+
+        }
+        if (rev> INT_MAX || rev < INT_MIN) {
+            return 0;
+        }
+        else{
+            return rev;
+        }
+    }
+};
